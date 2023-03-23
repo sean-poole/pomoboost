f6befbea348591ecef870f57f61267f8ca704907
Sean Poole <spoole5289@gmail.com>
2023-03-22 17:50:49 -0700

    configure for cyclic host

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
 
 const mainRoutes = require("./routes/main");
 const todoRoutes = require("./routes/todos");
@@ -15,7 +14,15 @@ require("dotenv").config({ path: "./config/.env" });
 
 require("./config/passport")(passport);
 
-connectDB();
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_STRING);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch(error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -40,6 +47,8 @@ app.use(flash());
 app.use("/", mainRoutes);
 app.use("/todos", todoRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running.`);
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running.`);
+    });
 });
